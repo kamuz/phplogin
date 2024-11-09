53596a303c7ce522840f9c6b018796e0b5c07eac
Vladimir Kamuz <v.kamuz@gmail.com>
2024-11-09 20:59:53 +0200

    Init

---
+++ phploginapp.sql
@@ -0,0 +1,15 @@
+SET NAMES utf8;
+
+DROP DATABASE IF EXISTS `phplogin`;
+CREATE DATABASE `phplogin`;
+USE `phplogin`;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
